e1b2715e3e562510750793f74047e662f41eb9b6
sskorol <serhii.s.korol@gmail.com>
2020-05-25 10:35:18 +0300

    Migrated to new allure-js and testdeck libraries. Upgraded outdated deps.

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -1,29 +1,36 @@
-import { step } from 'ts-test-decorators';
-import { User } from '../model/User';
-import { at } from '../utils/PageFactory';
-import GridPage from './GridPage';
-import Page from './Page';
+import { ContentType } from 'allure-js-commons'
+import { attachment, step } from 'ts-test-decorators'
+import { User } from '../model/User'
+import { at } from '../utils/PageFactory'
+import GridPage from './GridPage'
+import Page from './Page'
 
 export default class LoginPage extends Page {
   @step('Log into primary user account.')
   public loginWith(user: User): GridPage {
     return this.fillInUsername(user.username)
       .fillInPassword(user.password)
-      .clickSignInButton();
+      .clickSignInButton()
   }
 
   @step(username => `Fill in username: ${username}.`)
   public fillInUsername(username: string): LoginPage {
-    return this;
+    return this
   }
 
   @step(password => `Fill in password: ${password}.`)
   public fillInPassword(password: string): LoginPage {
-    return this;
+    return this
   }
 
   @step('Click "Sign In" button.')
   public clickSignInButton(): GridPage {
-    return at(GridPage);
+    this.loginAttachment()
+    return at(GridPage)
+  }
+
+  @attachment('Login Attachment', ContentType.TEXT)
+  private loginAttachment() {
+    return 'step attachment content'
   }
 }
